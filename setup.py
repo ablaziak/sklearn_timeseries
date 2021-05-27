6d8cfc559b9ae2a98271fdd05d8d5a2e78eb3c84
ablaziak <albert.blaziak@gmail.com>
2021-05-27 14:13:15 +0200

    configuration files added

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name='sklearn_timeseries',
+    packages=['sklearn_timeseries'],
+    version='0.1',
+    license='MIT',
+    description='Library helps to use timeseries sets properly with scikit-learn',
+    author='Albert',
+    author_email='albert.blaziak@gmail.com',
+    url='https://github.com/ablaziak/sklearn-timeseries',
+    download_url='https://github.com/ablaziak/sklearn-timeseries/archive/refs/tags/v0.1.tar.gz',
+    keywords=['timeseries', 'sklearn', 'scikit-learn', 'machine_learning'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
